82f0c3d35403ba46338e2d1bc2e9d929d94f3992
Ibad <bluespectreeye@gmail.com>
2026-09-12 21:39:33 +0500

    Add Grade Calculation

--- gradebook.cpp
+++ gradebook.cpp
@@ -21,5 +21,51 @@ cout << "Name: " << name << endl;
 cout << "ID: " << id << endl;
 cout << "Total marks: " << marks << endl;
 
+if(marks < 0 || marks > 100)
+{
+    cout << "Invalid marks" << endl;
+}
+
+else if(marks >= 90)
+{
+    cout << "Grade: A" << endl;
+}
+
+else if(marks >= 85)
+{
+    cout << "Grade: A-" << endl;
+}
+
+else if(marks >= 80)
+{
+    cout << "Grade: B" << endl;
+}
+
+else if(marks >= 70)
+{
+    cout << "Grade: B-" << endl;
+}
+
+else if(marks >= 60)
+{
+    cout << "Grade: C" << endl;
+}
+
+else if(marks >= 50)
+{
+    cout << "Grade: D" << endl;
+}
+
+else if(marks >= 40)
+{
+    cout << "Grade: D-" << endl;
+}
+
+else
+{
+    cout << "Fail" << endl;
+}
+
+
 return 0;
 }
